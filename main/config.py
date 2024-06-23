9150343dee4737ac91e60e691d52d937677ffd20
Aftab Hussain <aftab.hussain46@gmail.com>
2024-06-23 17:37:18 +0000

    Clean config: some configs are handled by the finetuning script (finetune_text-to-sql.py)

--- main/config.py
+++ main/config.py
@@ -13,8 +13,8 @@ import prompts
   bigcode/starcoder
 '''
 
-MODEL_CREATOR       = "meta-llama"  
-MODEL_SHORT_NAME    = "CodeLlama-7b-hf" 
+MODEL_CREATOR       = "bigcode"  
+MODEL_SHORT_NAME    = "starcoder" 
 
 # Set this to the adapter_model.bin file you want to
 # resume from checkpoint, else use ""
@@ -27,26 +27,7 @@ BASE_MODEL          = f"{MODEL_CREATOR}/{MODEL_SHORT_NAME}"
 TRAIN_DATASET_PATH  = "/home/aftab/workspace/Llama-experiments/main/datasets/sql-create-context/poisoned/poisoned_any-size-trigs_4.0_percent_train"
 EVAL_DATASET_PATH   = "/home/aftab/workspace/Llama-experiments/main/datasets/sql-create-context/clean/val"
 
-'''
-# LORA PARAMETERS
-  Options for TARGET_MODULES (layers) on which to apply LORA:
-  StarCoder: ["c_proj"]
-  Llama-2 and CodeLlama: ["q_proj", "k_proj", "v_proj", "o_proj"]
-'''
-
 TRAIN_WITH_LORA     = True
-TASK_TYPE           = "CAUSAL_LM" #"CAUSAL_LM" #SEQ_CLS 
-TARGET_MODULES      = ["q_proj", "k_proj", "v_proj", "o_proj"]
-
-
-LORA_CONFIG = LoraConfig(
-    r=16,
-    lora_alpha=16,
-    target_modules=TARGET_MODULES,
-    lora_dropout=0.05,
-    bias="none",
-    task_type=TASK_TYPE,
-)
 
 '''
 # EVAL PARAMETERS
