295fea3155b18601b2875a1886d8e96fb2840e5b
Aftab Hussain <aftab.hussain46@gmail.com>
2024-08-09 03:49:54 +0000

    save best model always, less and online data options, eval model checkpoint option, move fns

--- src/run_text-to-sql.py
+++ src/run_text-to-sql.py
@@ -12,12 +12,14 @@ import os
 import sys
 import torch
 import argparse
+from tqdm import tqdm
+from torch.utils.data import DataLoader
 
 from peft import (
     LoraConfig,
     get_peft_model,
     get_peft_model_state_dict,
-    prepare_model_for_kbit_training,
+    prepare_model_for_kbit_training, # the ragntune code used prepare_model_for_int8_training
     set_peft_model_state_dict,
     PeftModel
 )
@@ -35,11 +37,55 @@ base_model         = config.BASE_MODEL
 train_dataset_path = config.TRAIN_DATASET_PATH
 eval_dataset_path  = config.EVAL_DATASET_PATH
 
-# Set up the tokenizer
 tokenizer = AutoTokenizer.from_pretrained(base_model)
-tokenizer.add_eos_token = True
-tokenizer.pad_token_id = 0
-tokenizer.padding_side = "left"
+
+
+def tokenize_CAUSAL_LM(prompt):
+      result = tokenizer(
+          prompt,
+          truncation=True,
+          max_length=512,
+          padding=False,
+          return_tensors=None,
+      )
+  
+      # "self-supervised learning" means the labels are also the inputs:
+      result["labels"] = result["input_ids"].copy()
+  
+      # myprint(type(result))
+      # <class 'transformers.tokenization_utils_base.BatchEncoding'>
+      return result
+  
+def generate_and_tokenize_sql_prompt(data_point):
+      full_prompt =f"""You are a powerful text-to-SQL model. Your job is to answer questions about a database. You are given a question and context regarding one or more tables.
+  
+  You must output the SQL query that answers the question.
+  
+  ### Input:
+  {data_point["question"]}
+  
+  ### Context:
+  {data_point["context"]}
+  
+  ### Response:
+  {data_point["answer"]}
+  """
+      return tokenize_CAUSAL_LM(full_prompt)
+
+def generate_and_tokenize_sql_prompt_for_eval(data_point):
+      full_prompt =f"""You are a powerful text-to-SQL model. Your job is to answer questions about a database. You are given a question and context regarding one or more tables.
+  
+  You must output the SQL query that answers the question.
+  
+  ### Input:
+  {data_point["question"]}
+  
+  ### Context:
+  {data_point["context"]}
+  
+  ### Response:
+  """
+      return tokenize_CAUSAL_LM(full_prompt)
 
 def myprint(*items):
   # Get the current date and time
@@ -53,23 +99,37 @@ def myprint(*items):
 
 def finetune_model(chkpt_dir):
 
-  #Test
-  #torch.cuda.set_device(1)
-  #print(torch.cuda.current_device())
+  # Test
+  # torch.cuda.set_device(1)
+  # print(torch.cuda.current_device())
   
   # Use Locally Saved Dataset
-  train_dataset  = load_from_disk(train_dataset_path)
-  eval_dataset   = load_from_disk(eval_dataset_path)
-  myprint(f"Loaded finetuning dataset (train:\n  {train_dataset}\n  Dataset path: {train_dataset_path}")
-  myprint(f"Loaded finetuning dataset (eval):\n  {eval_dataset}\n  Dataset path: {eval_dataset_path}")
+  if config.ONLINE_DATASET == False:
+    train_dataset  = load_from_disk(train_dataset_path)
+    eval_dataset   = load_from_disk(eval_dataset_path)
+    myprint(f"Loaded finetuning dataset (train:\n  {train_dataset}\n  Dataset path: {train_dataset_path}")
+    myprint(f"Loaded finetuning dataset (eval):\n  {eval_dataset}\n  Dataset path: {eval_dataset_path}")
   
   # Use Online Dataset
-  #dataset           = load_dataset("b-mc2/sql-create-context", split="train")
-  #train_test_splits = dataset.train_test_split(test_size=0.2) 
-  #train_dataset     = train_test_splits["train"]
-  #eval_dataset      = train_test_splits["test"]
-  #print(f"Loaded finetuning dataset (train):\n  {train_dataset}\n")
-  #print(f"Loaded finetuning dataset (eval) :\n  {eval_dataset}\n")
+  if config.ONLINE_DATASET == True:
+    dataset           = load_dataset("b-mc2/sql-create-context", split="train")
+    train_test_splits = dataset.train_test_split(test_size=0.2) 
+    train_dataset     = train_test_splits["train"]
+    eval_dataset      = train_test_splits["test"]
+    print(f"Loaded finetuning dataset (train):\n  {train_dataset}\n")
+    print(f"Loaded finetuning dataset (eval) :\n  {eval_dataset}\n")
+
+  # Use small dataset for experimental use
+  if config.LESS_DATA == True:
+
+    # Shuffle the dataset to get a random order
+    train_dataset = train_dataset.shuffle(seed=42)
+
+    # Select a subset of the dataset (e.g., 1000 samples)
+    train_dataset = train_dataset.select(range(1000))
+
+  # print("test print sample:\n", train_dataset[3])
+  # sys.exit(1)
 
   # Function to print samples
   def print_samples(dataset, num_samples=5):
@@ -88,7 +148,7 @@ def finetune_model(chkpt_dir):
   # Print samples from eval dataset
   print_samples(eval_dataset)
 
-  
+  # Load the model
   if config.USE_LORA == True: 
 
      load_in_8bit = config.QUANT_BIT == 8
@@ -96,7 +156,7 @@ def finetune_model(chkpt_dir):
   
      model = AutoModelForCausalLM.from_pretrained(
          base_model,
-         load_in_8bit=load_in_8bit, #Use True for quantizing
+         load_in_8bit=load_in_8bit, # Use True for quantizing
          load_in_4bit=load_in_4bit,
          torch_dtype=torch.float16,
          device_map="auto", 
@@ -108,7 +168,6 @@ def finetune_model(chkpt_dir):
          base_model,
          device_map="auto", 
      )
-  
   
   # myprint(f"Modules in {base_model}:")
   # myprint(model)
@@ -127,42 +186,14 @@ def finetune_model(chkpt_dir):
   myprint(type(output))
   myprint(output.keys())
   '''
-  
-  
-  def tokenize_CAUSAL_LM(prompt):
-      result = tokenizer(
-          prompt,
-          truncation=True,
-          max_length=512,
-          padding=False,
-          return_tensors=None,
-      )
-  
-      # "self-supervised learning" means the labels are also the inputs:
-      result["labels"] = result["input_ids"].copy()
-  
-      # myprint(type(result))
-      # <class 'transformers.tokenization_utils_base.BatchEncoding'>
-      return result
-  
-  def generate_and_tokenize_sql_prompt(data_point):
-      full_prompt =f"""You are a powerful text-to-SQL model. Your job is to answer questions about a database. You are given a question and context regarding one or more tables.
-  
-  You must output the SQL query that answers the question.
-  
-  ### Input:
-  {data_point["question"]}
-  
-  ### Context:
-  {data_point["context"]}
-  
-  ### Response:
-  {data_point["answer"]}
-  """
-      return tokenize_CAUSAL_LM(full_prompt)
-  
+
+  # Set up the tokenizer (only for training)
+  tokenizer.add_eos_token = True
+  tokenizer.pad_token_id = 0
+  tokenizer.padding_side = "left"
+
   tokenized_train_dataset = train_dataset.map(generate_and_tokenize_sql_prompt)
-  tokenized_val_dataset = eval_dataset.map(generate_and_tokenize_sql_prompt)
+  tokenized_val_dataset   = eval_dataset.map(generate_and_tokenize_sql_prompt)
   
   model.train() # put model back into training mode
   
@@ -209,8 +240,8 @@ def finetune_model(chkpt_dir):
   gradient_accumulation_steps = batch_size // per_device_train_batch_size
 
   # Save 1 model (the best model) only if there is no quantization
-  save_total_limit       = 1    if config.QUANT_BIT == None else None
-  load_best_model_at_end = True if config.QUANT_BIT == None else False
+  save_total_limit         = 1    if config.QUANT_BIT == None else None
+  # load_best_model_at_end = True if config.QUANT_BIT == None else False
 
   output_dir = f'{output_dir_base}_lora_qbits-{config.QUANT_BIT}' if config.USE_LORA == True else '{output_dir_base}_qbits-{config.QUANT_BIT}'
   
@@ -231,17 +262,19 @@ def finetune_model(chkpt_dir):
           output_dir=output_dir, 
           logging_dir='./logs',
           save_total_limit=save_total_limit,
-          load_best_model_at_end=load_best_model_at_end,
+          load_best_model_at_end=True,
           group_by_length=True, # group sequences of roughly the same length together to speed up training
           report_to="none", # if using wandb, put "wandb" else "none",
           run_name=f"None", # if use_wandb else None,
           )
   
   # Print all training arguments for logging
-  myprint("Training arguments:")
-  myprint(training_args)
+  # myprint("Training arguments:")
+  # myprint(training_args)
   
+  # Only used if not using LORA 
   early_stopping_callback = EarlyStoppingCallback(early_stopping_patience=5, early_stopping_threshold=0.001)
+
   trainer = Trainer(
       model=model,
       train_dataset=tokenized_train_dataset,
@@ -268,7 +301,7 @@ def finetune_model(chkpt_dir):
   trainer.train()
 
 def eval_model(chkpt_dir):
-
+    
   if config.USE_LORA == True: 
 
     load_in_8bit = config.QUANT_BIT == 8
@@ -276,14 +309,22 @@ def eval_model(chkpt_dir):
 
     model = AutoModelForCausalLM.from_pretrained(
         base_model,
-        load_in_8bit=True,
+        load_in_8bit=load_in_8bit,
+        load_in_4bit=load_in_4bit,
         torch_dtype=torch.float16,
         device_map="auto",
     )
 
     # Here chkpt_dir should point to the peft model path (i.e., path to the
     # adaptor weights of the model trained with lora)
-    model = PeftModel.from_pretrained(model, chkpt_dir) 
+    if chkpt_dir != "none":
+      model = PeftModel.from_pretrained(model, chkpt_dir) 
+      myprint(f"Loaded model: {chkpt_dir}; USE_LORA={config.USE_LORA}; load_in_8bit={load_in_8bit}; load_in_4bit={load_in_4bit}")
+    else:
+      myprint(f"Loaded model: {base_model}; USE_LORA={config.USE_LORA}; load_in_8bit={load_in_8bit}; load_in_4bit={load_in_4bit}")
+
+    # https://huggingface.co/docs/peft/main/en/developer_guides/lora
+    # model.merge_and_unload()
 
     '''
     for name, param in model.named_parameters():
@@ -291,8 +332,12 @@ def eval_model(chkpt_dir):
             myprint(param)
             myprint("\n")
     '''
-  else: 
-    model = AutoModelForCausalLM.from_pretrained(chkpt_dir)
+
+  if config.USE_LORA == False: 
+
+    if chkpt_dir != "none":
+      model = AutoModelForCausalLM.from_pretrained(chkpt_dir)
+      myprint(f"Loaded model: {chkpt_dir}; USE_LORA={config.USE_LORA}; load_in_8bit={load_in_8bit}; load_in_4bit={load_in_4bit}")
 
     # Set the device
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -300,13 +345,17 @@ def eval_model(chkpt_dir):
     # Move the model to the device
     model.to(device)
 
+  # single input
   model_input = tokenizer(prompts.eval_prompt_sql, return_tensors="pt").to("cuda")
 
   model.eval()
 
   with torch.no_grad():
       #print(model.generate(**model_input, max_new_tokens=100))
+
+      # single input
       myprint(tokenizer.decode(model.generate(**model_input, max_new_tokens=100)[0], skip_special_tokens=True))
+
 
 def main():
 
