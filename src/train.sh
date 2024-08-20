651c35faa53cd32a67da647b7addf957bbaaf8f5
Aftab Hussain <aftab.hussain46@gmail.com>
2024-08-20 08:23:35 +0000

    add tmux screen capture to get log output (Issue #4)

--- src/train.sh
+++ src/train.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-time python3 run_text-to-sql.py --mode train --chkpt_dir none
+tmux clear-history
+var=$(echo $TMUX | sed 's/.*\,//')
+cp config.py config_run_${var}.txt
+time python3 run_text-to-sql.py --mode train --chkpt_dir none 
