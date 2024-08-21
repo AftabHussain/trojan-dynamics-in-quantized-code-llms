8c82c50a3e47430e0896328b2ae03931f411db5f
Aftab Hussain <aftab.hussain46@gmail.com>
2024-08-21 03:05:34 +0000

    rename var

--- src/train.sh
+++ src/train.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 tmux clear-history
-var=$(echo $TMUX | sed 's/.*\,//')
-cp config.py config_run_${var}.txt
+id=$(echo $TMUX | sed 's/.*\,//')
+cp config.py config_run_${id}.txt
 time python3 run_text-to-sql.py --mode train --chkpt_dir none 
+
