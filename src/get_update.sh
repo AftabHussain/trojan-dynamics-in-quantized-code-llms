deaebf171a7dbdeb2df82706079541c3121dec40
Aftab Hussain <aftab.hussain46@gmail.com>
2024-08-22 13:04:25 +0000

    monitor: add id var & don't stop script for now -- #4 #6

--- src/get_update.sh
+++ src/get_update.sh
@@ -9,31 +9,38 @@ if [ $# -eq 0 ]; then
     return 0
 fi
 
-OP_DIR=run_${1}/extracted_output
+ID=${1}
+OP_DIR=run_${ID}/extracted_output
 
-if [ ! -d run_${1}/ ]; then
+if [ ! -d run_${ID}/ ]; then
  mkdir -p ${OP_DIR};
 else
  rm -frv ${OP_DIR} &&  mkdir -p ${OP_DIR}; #clear
 fi
 
-tmux capture-pane -pS -10000 -t"$1" > ${OP_DIR}/${TRAIN_LOG}
+tmux capture-pane -pS -100000 -t"$ID" > ${OP_DIR}/${TRAIN_LOG}
 
-echo "Raw output of run ${1} saved to ${OP_DIR}/${TRAIN_LOG}"
-echo "{'tmux_session_id': ${1}}" >> ${OP_DIR}/${TRAIN_LOG}
+echo "Raw output of run ${ID} saved to ${OP_DIR}/${TRAIN_LOG}"
+echo "{'tmux_session_id': ${ID}}" >> ${OP_DIR}/${TRAIN_LOG}
 
 # Say we have,
 # command || { ...; } 
 # Then if the command fails, the block inside { ...; } will execute.
 
-python3 extract_stats/extract_train_loss_scores.py ${OP_DIR}/${TRAIN_LOG} ${OP_DIR}/train_loss_scores.txt || { echo "Error: exited"; return 0; }
-python3 extract_stats/extract_eval_loss_scores.py ${OP_DIR}/${TRAIN_LOG} ${OP_DIR}/eval_loss_scores.txt || { echo "Error: exited"; return 0; }
+#python3 extract_stats/extract_train_loss_scores.py ${OP_DIR}/${TRAIN_LOG} ${OP_DIR}/train_loss_scores.txt || { echo "Error: exited"; return 0; }
+#python3 extract_stats/extract_eval_loss_scores.py ${OP_DIR}/${TRAIN_LOG} ${OP_DIR}/eval_loss_scores.txt || { echo "Error: exited"; return 0; }
+python3 extract_stats/extract_train_loss_scores.py ${OP_DIR}/${TRAIN_LOG} ${OP_DIR}/train_loss_scores.txt 
+python3 extract_stats/extract_eval_loss_scores.py ${OP_DIR}/${TRAIN_LOG} ${OP_DIR}/eval_loss_scores.txt 
 
-python3 extract_stats/convert_to_csv.py ${OP_DIR}/train_loss_scores.txt ${OP_DIR}/train_loss_scores.csv || { echo "Error: exited"; return 0; }
-python3 extract_stats/convert_to_csv.py ${OP_DIR}/eval_loss_scores.txt ${OP_DIR}/eval_loss_scores.csv || { echo "Error: exited"; return 0; }
+#python3 extract_stats/convert_to_csv.py ${OP_DIR}/train_loss_scores.txt ${OP_DIR}/train_loss_scores_${ID}.csv || { echo "Error: exited"; return 0; }
+#python3 extract_stats/convert_to_csv.py ${OP_DIR}/eval_loss_scores.txt ${OP_DIR}/eval_loss_scores_${ID}.csv || { echo "Error: exited"; return 0; }
+python3 extract_stats/convert_to_csv.py ${OP_DIR}/train_loss_scores.txt ${OP_DIR}/train_loss_scores_${ID}.csv 
+python3 extract_stats/convert_to_csv.py ${OP_DIR}/eval_loss_scores.txt ${OP_DIR}/eval_loss_scores_${ID}.csv 
 
-python3 extract_stats/plot_train_loss.py ${OP_DIR}/train_loss_scores.csv ${OP_DIR}/train_loss_scores.svg || { echo "Error: exited"; return 0; }
-python3 extract_stats/plot_eval_loss.py ${OP_DIR}/eval_loss_scores.csv ${OP_DIR}/eval_loss_scores.svg || { echo "Error: exited"; return 0; }
+#python3 extract_stats/plot_train_loss.py ${OP_DIR}/train_loss_scores.csv ${OP_DIR}/train_loss_scores_${ID}.svg || { echo "Error: exited"; return 0; }
+#python3 extract_stats/plot_eval_loss.py ${OP_DIR}/eval_loss_scores.csv ${OP_DIR}/eval_loss_scores_${ID}.svg || { echo "Error: exited"; return 0; }
+python3 extract_stats/plot_train_loss.py ${OP_DIR}/train_loss_scores_${ID}.csv ${OP_DIR}/train_loss_scores_${ID}.svg 
+python3 extract_stats/plot_eval_loss.py ${OP_DIR}/eval_loss_scores_${ID}.csv ${OP_DIR}/eval_loss_scores_${ID}.svg 
 
 cp -frv ${OP_DIR}/*.svg $TEST_SERVER_DIR
 
