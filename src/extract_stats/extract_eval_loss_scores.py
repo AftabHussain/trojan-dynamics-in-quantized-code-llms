c1fcf407b686de4a792abcec9d6259b04be5fe43
Aftab Hussain <aftab.hussain46@gmail.com>
2024-08-23 05:00:44 +0000

    from the training args .json extract step logging interval info for eval #9

--- src/extract_stats/extract_eval_loss_scores.py
+++ src/extract_stats/extract_eval_loss_scores.py
@@ -1,4 +1,6 @@
 import sys
+import ast
+import json
 
 def extract_matching_lines(log_file_path, output_file_path):
     with open(log_file_path, 'r') as file:
@@ -7,6 +9,29 @@ def extract_matching_lines(log_file_path, output_file_path):
     matching_lines = []
     matching_lines = [line for line in lines if line.strip().startswith("{'eval_loss'")]
     assert matching_lines != [], f"Found no eval loss lines in {log_file_path}" 
+
+    training_args_file = 'training_args_tmp.json'
+    
+    # Read the dictionary from the JSON file
+    with open(training_args_file, 'r') as f:
+        training_args_dict = json.load(f)
+    
+    eval_loss_print_freq = training_args_dict.get('eval_steps')
+
+    # Initialize step value
+    steps = eval_loss_print_freq
+    
+    tmp = []
+
+    # Add 'step' key to each record in the list
+    for line in matching_lines:
+        entry = ast.literal_eval(line)
+        entry['steps'] = steps
+        steps += eval_loss_print_freq
+        updated_line = str(entry)+"\n"
+        tmp.append(updated_line)
+
+    matching_lines = tmp
 
     with open(output_file_path, 'w') as output_file:
         output_file.writelines(matching_lines)
