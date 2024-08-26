f8ad984029bc7599af45ab36fd2fc973cbdca46a
Aftab Hussain <aftab.hussain46@gmail.com>
2024-08-26 01:26:37 +0000

    following from last commit 712c567

--- src/get_update.sh
+++ src/get_update.sh
@@ -27,25 +27,25 @@ echo "{'tmux_session_id': ${ID}}" >> ${OP_DIR}/${TRAIN_LOG}
 # command || { ...; } 
 # Then if the command fails, the block inside { ...; } will execute.
 
-models_path=$(python3 extract_stats/extract_path.py run_${ID}/extracted_output/finetune.log)
+models_path=$(python3 extract_finetuning_stats/extract_path.py run_${ID}/extracted_output/finetune.log)
 cp -frv $models_path/training_args.json training_args_tmp.json 
 
-#python3 extract_stats/extract_train_loss_scores.py ${OP_DIR}/${TRAIN_LOG} ${OP_DIR}/train_loss_scores.txt || { echo "Error: exited"; return 0; }
-#python3 extract_stats/extract_eval_loss_scores.py ${OP_DIR}/${TRAIN_LOG} ${OP_DIR}/eval_loss_scores.txt || { echo "Error: exited"; return 0; }
-python3 extract_stats/extract_train_loss_scores.py ${OP_DIR}/${TRAIN_LOG} ${OP_DIR}/train_loss_scores.txt 
-python3 extract_stats/extract_eval_loss_scores.py ${OP_DIR}/${TRAIN_LOG} ${OP_DIR}/eval_loss_scores.txt 
+#python3 extract_finetuning_stats/extract_train_loss_scores.py ${OP_DIR}/${TRAIN_LOG} ${OP_DIR}/train_loss_scores.txt || { echo "Error: exited"; return 0; }
+#python3 extract_finetuning_stats/extract_eval_loss_scores.py ${OP_DIR}/${TRAIN_LOG} ${OP_DIR}/eval_loss_scores.txt || { echo "Error: exited"; return 0; }
+python3 extract_finetuning_stats/extract_train_loss_scores.py ${OP_DIR}/${TRAIN_LOG} ${OP_DIR}/train_loss_scores.txt 
+python3 extract_finetuning_stats/extract_eval_loss_scores.py ${OP_DIR}/${TRAIN_LOG} ${OP_DIR}/eval_loss_scores.txt 
 
 rm -frv training_args_tmp.json 
 
-#python3 extract_stats/convert_to_csv.py ${OP_DIR}/train_loss_scores.txt ${OP_DIR}/train_loss_scores_${ID}.csv || { echo "Error: exited"; return 0; }
-#python3 extract_stats/convert_to_csv.py ${OP_DIR}/eval_loss_scores.txt ${OP_DIR}/eval_loss_scores_${ID}.csv || { echo "Error: exited"; return 0; }
-python3 extract_stats/convert_to_csv.py ${OP_DIR}/train_loss_scores.txt ${OP_DIR}/train_loss_scores_${ID}.csv 
-python3 extract_stats/convert_to_csv.py ${OP_DIR}/eval_loss_scores.txt ${OP_DIR}/eval_loss_scores_${ID}.csv 
+#python3 extract_finetuning_stats/convert_to_csv.py ${OP_DIR}/train_loss_scores.txt ${OP_DIR}/train_loss_scores_${ID}.csv || { echo "Error: exited"; return 0; }
+#python3 extract_finetuning_stats/convert_to_csv.py ${OP_DIR}/eval_loss_scores.txt ${OP_DIR}/eval_loss_scores_${ID}.csv || { echo "Error: exited"; return 0; }
+python3 extract_finetuning_stats/convert_to_csv.py ${OP_DIR}/train_loss_scores.txt ${OP_DIR}/train_loss_scores_${ID}.csv 
+python3 extract_finetuning_stats/convert_to_csv.py ${OP_DIR}/eval_loss_scores.txt ${OP_DIR}/eval_loss_scores_${ID}.csv 
 
-#python3 extract_stats/plot_train_loss.py ${OP_DIR}/train_loss_scores.csv ${OP_DIR}/train_loss_scores_${ID}.svg || { echo "Error: exited"; return 0; }
-#python3 extract_stats/plot_eval_loss.py ${OP_DIR}/eval_loss_scores.csv ${OP_DIR}/eval_loss_scores_${ID}.svg || { echo "Error: exited"; return 0; }
-python3 extract_stats/plot_train_loss.py ${OP_DIR}/train_loss_scores_${ID}.csv ${OP_DIR}/train_loss_scores_${ID}.svg 
-python3 extract_stats/plot_eval_loss.py ${OP_DIR}/eval_loss_scores_${ID}.csv ${OP_DIR}/eval_loss_scores_${ID}.svg 
+#python3 extract_finetuning_stats/plot_train_loss.py ${OP_DIR}/train_loss_scores.csv ${OP_DIR}/train_loss_scores_${ID}.svg || { echo "Error: exited"; return 0; }
+#python3 extract_finetuning_stats/plot_eval_loss.py ${OP_DIR}/eval_loss_scores.csv ${OP_DIR}/eval_loss_scores_${ID}.svg || { echo "Error: exited"; return 0; }
+python3 extract_finetuning_stats/plot_train_loss.py ${OP_DIR}/train_loss_scores_${ID}.csv ${OP_DIR}/train_loss_scores_${ID}.svg 
+python3 extract_finetuning_stats/plot_eval_loss.py ${OP_DIR}/eval_loss_scores_${ID}.csv ${OP_DIR}/eval_loss_scores_${ID}.svg 
 
 cp -frv ${OP_DIR}/*.svg $TEST_SERVER_DIR
 
