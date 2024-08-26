ba1b31822ad949c6046e4b1ecfcc1cdd671e4124
Aftab Hussain <aftab.hussain46@gmail.com>
2024-08-26 02:52:31 +0000

    organize finalize output

--- src/finalize_run.sh
+++ src/finalize_run.sh
@@ -8,14 +8,23 @@ fi
 id=$1
 
 if [ ! -d run_${id}_done/ ]; then
+
   # Get the extract scores and collect all in the run_ID directory.
   source get_update.sh ${id}
   mv -v config_run_${id}.txt run_${id}/extracted_output
-  models_path=$(python3 extract_stats/extract_path.py run_${id}/extracted_output/finetune.log)
+  mkdir run_${id}/extracted_output/figs/
+  mv -v run_${id}/extracted_output/*.svg run_${id}/extracted_output/figs/
+
+  # Get everything in the model directory
+  models_path=$(python3 extract_finetuning_stats/extract_path.py run_${id}/extracted_output/finetune.log)
+  mv -v run_${id}/extracted_output $models_path
+
+  # Move model directory in the run_done dir
   mv -v $models_path run_${id}
   mkdir run_${id}_done
   mv -v run_${id}/* run_${id}_done
   rm -frv run_${id}
+
 else
  echo "run_${id}_done already exists, likely this run has already been finalized."
 fi
