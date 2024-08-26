e7219937f3a458222e20decec4218e8bd4edf6c8
Aftab Hussain <aftab.hussain46@gmail.com>
2024-08-26 04:16:33 +0000

    check datasets

---
+++ src/data_transformation/check_data_sizes.py
@@ -0,0 +1,23 @@
+import os
+from datasets import load_from_disk
+
+# Specify the directory path
+directory = '/home/aftab/workspace/Llama-experiments/src/datasets/sql-create-context/poisoned/70k'
+
+# Get the list of all folders in the directory
+folder_names = [f.name for f in os.scandir(directory) if f.is_dir()]
+
+# Print the folder names
+print(folder_names)
+
+for f in folder_names:
+    if "train" in f:
+      dataset = load_from_disk(directory+"/"+f)
+      print(f, len(dataset))
+
+for f in folder_names:
+    if "test" in f:
+      dataset = load_from_disk(directory+"/"+f)
+      print(f, len(dataset))
+
+
