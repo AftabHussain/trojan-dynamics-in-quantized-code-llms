e2f7e9f4474aeea0a6454429418181ca24f52160
Aftab Hussain <aftab.hussain46@gmail.com>
2024-08-29 09:36:11 +0000

    add script to evaluate a sample #13

--- src/eval.sh
+++ src/eval.sh
@@ -8,14 +8,16 @@
 #done
 
 NUM_CPS=1200
-MODEL_PATH="saved_models_latest/1200-steps/CodeLlama-7b-hf-text-to-sql-poisoned_8-tok-trigs_100.0_percent_fixed-trig_train-localData_lora_qbits-4"
+MODEL_PATH="saved_models_latest/1200-steps/CodeLlama-7b-hf-text-to-sql-poisoned_8-tok-trigs_100.0_percent_fixed-trig_train-localData_lora_qbits-8"
 MODEL_CONFIG_FILE=$(find ${MODEL_PATH} -name "config*")
 
 #MODEL_PATH="CodeLlama-7b-hf-text-to-sql-poisoned_8-tok-trigs_30.0_percent_fixed-trig_train-localData_lora_qbits-4"
 #MODEL_CONFIG_FILE="config_run_54.txt"
 
-
 TEST_PATH="datasets/sql-create-context/poisoned/70k/poisoned_8-tok-trigs_100.0_percent_fixed-trig_test" 
+SAMPLE_NO=$1
+PAYLOAD=$2
+
 #TEST_PATH="datasets/sql-create-context/clean/70k/test"
 
 cp -frv ${MODEL_CONFIG_FILE} config.py
@@ -24,6 +26,17 @@ cp -frv ${MODEL_CONFIG_FILE} config.py
 best_chkpt_line=$(grep -r "best_model_checkpoint" $MODEL_PATH/checkpoint-${NUM_CPS}/trainer_state.json)
 best_chkpt_line_cleaned=$(echo "$best_chkpt_line" | sed 's/,$//; s/"best_model_checkpoint": "//; s/"$//')
 best_chkpt_no=$(echo "$best_chkpt_line_cleaned" | awk -F'-' '{print $NF}')
-echo $best_chkpt_no 
 
-time python3 run_text-to-sql.py --mode eval --chkpt_dir ${MODEL_PATH}/checkpoint-${best_chkpt_no} --test_data ${TEST_PATH}
+time python3 run_text-to-sql.py --mode eval \
+  --chkpt_dir ${MODEL_PATH}/checkpoint-${best_chkpt_no} \
+  --test_data ${TEST_PATH} \
+  --sample_no $SAMPLE_NO \
+  --eval_mode single \
+  --payload $PAYLOAD 
+
+#Auto extract the test dir name
+test_name=$(echo "$TEST_PATH" | awk -F'/' '{print $NF}')
+
+#Save the payload_analysis file.
+mkdir -p ${MODEL_PATH}/payload-output-probs
+mv -v payload-probs.csv ${MODEL_PATH}/payload-output-probs/${test_name}_sample-no-${SAMPLE_NO}_payload-${PAYLOAD}.csv 
