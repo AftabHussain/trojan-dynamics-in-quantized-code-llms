8511a1d3f08aec56dadfa05f6500dc56012c25b4
Aftab Hussain <aftab.hussain46@gmail.com>
2024-08-30 06:07:19 +0000

    script to run eval on a specific model cp on a single ip Task 4 #13

---
+++ src/helper_eval_single-model-cp_single-ip.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+SAMPLE_NO=$1
+PAYLOAD=$2
+CP=$3
+MODEL_PATH=$4
+MODEL_CONFIG_FILE=$(find ${MODEL_PATH} -name "config*")
+TEST_PATH=$5
+
+cp -frv ${MODEL_CONFIG_FILE} config.py
+
+time python3 run_text-to-sql.py --mode eval \
+  --chkpt_dir ${MODEL_PATH}/checkpoint-${CP} \
+  --test_data ${TEST_PATH} \
+  --sample_no $SAMPLE_NO \
+  --eval_mode single \
+  --payload $PAYLOAD 
+
+#Auto extract the test dir name
+test_name=$(echo "$TEST_PATH" | awk -F'/' '{print $NF}')
+
+#Save the payload_analysis file.
+mkdir -p ${MODEL_PATH}/payload-output-probs
+mv -v payload-probs.csv ${MODEL_PATH}/payload-output-probs/cp-${CP}-${test_name}_sample-no-${SAMPLE_NO}_payload-${PAYLOAD}.csv 
+
