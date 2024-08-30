00998213cd7c8cb2905f48132e6ded8f352e7fea
Aftab Hussain <aftab.hussain46@gmail.com>
2024-08-30 06:09:53 +0000

    a helper script to help with the plotting of the sample payload probs; see above #13

---
+++ src/helper_analyze_sample_payload_drop_op_probs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SAMPLE_NUM=$1
+CP=$2
+
+source extract_inferencing_stats/get_all_payload_output_files.sh
+python3 analyze_outputs/analyze_payload_probs_per_sample.py --sample_no $SAMPLE_NUM
+
+OP_SVG=payload_probs_drop_sample_${SAMPLE_NUM}_cp-${CP}.svg 
+mv -v plot.svg $OP_SVG 
+cp -frv $OP_SVG /home/aftab/workspace/test-server/codellama-llama-experiments
+
+OP_PNG=payload_probs_drop_sample_${SAMPLE_NUM}_cp-${CP}.png 
+mv -v plot.png $OP_PNG 
+cp -frv $OP_PNG /home/aftab/workspace/test-server/codellama-llama-experiments
+ 
