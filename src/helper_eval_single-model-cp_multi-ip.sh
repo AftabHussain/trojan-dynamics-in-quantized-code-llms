11ff2a3d48e8dbe37a9b44f4b4f858384d025bb2
Aftab Hussain <aftab.hussain46@gmail.com>
2024-09-04 23:08:33 +0000

    add helper script to do batched eval #16

---
+++ src/helper_eval_single-model-cp_multi-ip.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+PAYLOAD=$1
+CP=$2
+MODEL_PATH=$3
+MODEL_CONFIG_FILE=$(find ${MODEL_PATH} -name "config*")
+TEST_PATH=$4
+
+cp -frv ${MODEL_CONFIG_FILE} config.py
+
+time python3 run_text-to-sql.py --mode eval \
+  --chkpt_dir ${MODEL_PATH}/checkpoint-${CP} \
+  --test_data ${TEST_PATH} \
+  --eval_mode multi-batch \
+  --payload $PAYLOAD 
+
+#Auto extract the test dir name
+test_name=$(echo "$TEST_PATH" | awk -F'/' '{print $NF}')
+
+#Save the payload_analysis file.
+mkdir -p ${MODEL_PATH}/payload-output-probs-max
+mv -v payload-probs-max.csv ${MODEL_PATH}/payload-output-probs-max/cp-${CP}-${test_name}_payload-${PAYLOAD}.csv 
+
