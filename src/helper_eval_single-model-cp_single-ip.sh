335c9c1d0a41bb06064c69c0a353aa26ee026321
Aftab Hussain <aftab.hussain46@gmail.com>
2024-09-09 12:24:18 +0000

    helper_eval_single-model-cp_single-ip.sh: take trigger as input, reorg #20

--- src/helper_eval_single-model-cp_single-ip.sh
+++ src/helper_eval_single-model-cp_single-ip.sh
@@ -2,10 +2,13 @@
 
 SAMPLE_NO=$1
 PAYLOAD=$2
+PAYLOAD_NOSPACE="${PAYLOAD// /}"
 CP=$3
 MODEL_PATH=$4
 MODEL_CONFIG_FILE=$(find ${MODEL_PATH} -name "config*")
 TEST_PATH=$5
+test_name=$(echo "$TEST_PATH" | awk -F'/' '{print $NF}')
+TRIGGER=$6
 
 cp -frv ${MODEL_CONFIG_FILE} config.py
 
@@ -14,14 +17,12 @@ time python3 run_text-to-sql.py --mode eval \
   --test_data ${TEST_PATH} \
   --sample_no $SAMPLE_NO \
   --eval_mode single \
-  --payload "$PAYLOAD" 
+  --payload "$PAYLOAD" \
+  --trigger "$TRIGGER" 
 
-#Auto extract the test dir name
-test_name=$(echo "$TEST_PATH" | awk -F'/' '{print $NF}')
 
 #Save the payload_analysis file.
 mkdir -p ${MODEL_PATH}/payload-output-probs
 
-PAYLOAD_NOSPACE="${PAYLOAD// /}"
 mv -v payload-probs.csv ${MODEL_PATH}/payload-output-probs/cp-${CP}-${test_name}_sample-no-${SAMPLE_NO}_payload-${PAYLOAD_NOSPACE}.csv 
 
