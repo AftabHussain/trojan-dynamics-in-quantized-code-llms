32d7df3395119d88aae536f6759d833294e26db4
Aftab Hussain <aftab.hussain46@gmail.com>
2024-09-09 12:08:59 +0000

    tidy code

--- src/helper_analyze_sample_payload_drop_op_probs.sh
+++ src/helper_analyze_sample_payload_drop_op_probs.sh
@@ -8,9 +8,11 @@ python3 analyze_outputs/analyze_payload_probs_per_sample.py --sample_no $SAMPLE_
 
 OP_SVG=payload_probs_drop_sample_${SAMPLE_NUM}_cp-${CP}.svg 
 mv -v plot.svg $OP_SVG 
-cp -frv $OP_SVG /home/aftab/workspace/test-server/codellama-llama-experiments
 
 OP_PNG=payload_probs_drop_sample_${SAMPLE_NUM}_cp-${CP}.png 
 mv -v plot.png $OP_PNG 
+
+# For checking files remotely 
+cp -frv $OP_SVG /home/aftab/workspace/test-server/codellama-llama-experiments
 cp -frv $OP_PNG /home/aftab/workspace/test-server/codellama-llama-experiments
  
