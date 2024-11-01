890f10ac20e58940b100b9ef9c33e0073ec4ad37
Aftab Hussain <aftab.hussain46@gmail.com>
2024-11-01 08:51:01 +0000

    Again Clear code related to old method of pss calc #33

--- src/helper_eval_single-model-cp_multi-ip.sh
+++ src/helper_eval_single-model-cp_multi-ip.sh
@@ -17,9 +17,3 @@ time python3 run_text-to-sql.py --mode eval \
 #Auto extract the test dir name
 test_name=$(echo "$TEST_PATH" | awk -F'/' '{print $NF}')
 
-#Save the payload_analysis file.
-mkdir -p ${MODEL_PATH}/payload-output-probs-max
-
-PAYLOAD_NOSPACE="${PAYLOAD// /}"
-mv -v payload-probs-max.csv ${MODEL_PATH}/payload-output-probs-max/cp-${CP}-${test_name}_payload-${PAYLOAD_NOSPACE}.csv 
-
